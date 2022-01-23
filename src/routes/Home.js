24c627cb943bce45695c369a38ea8296d83c5fa5
reader-wh94 <sstt9884@gmail.com>
2022-01-23 22:08:16 +0900

    Set Sweet form and database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [sweet, setSweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: {value},
+    } = event;
+    setSweet(value);
+  };
+
+  return (
+    <div>
+    <form>
+      <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+      <input type="submit" value="Sweet" />
+    </form>
+  </div>
+
+  );
+}
 
 export default Home;
