223e054d7f4425f7fc5e9a7f132b4a5fc5383859
reader-wh94 <sstt9884@gmail.com>
2022-01-25 20:49:57 +0900

    Get the Sweets on screen

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
   const [sweet, setSweet] = useState("");
+  const [sweets, setSweets] = useState([]);
   
+  const getSweets = async() => {
+    const dbSweets = await dbService.collection("sweets").get();
+    
+    dbSweets.forEach((document) => {
+      const sweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setSweets((prev) => [sweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+   getSweets();
+  }, []);
+
   const onSubmit = async(event) => {
     event.preventDefault();
     await dbService.collection("sweets").add({
@@ -19,6 +36,7 @@ const Home = () => {
     } = event;
     setSweet(value);
   };
+  console.log(sweets);
 
   return (
     <div>
@@ -26,6 +44,13 @@ const Home = () => {
       <input type="text" onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
       <input type="submit" value="Sweet" />
     </form>
+    <div>
+      {sweets.map((sweet) =>
+        <div key={sweet.id}>
+          <h4>{sweet.sweet}</h4>
+        </div>
+      )}
+    </div>
   </div>
 
   );
