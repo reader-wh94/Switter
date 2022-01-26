5f1551b2c923b8539975a5933895efa0be0e0b4d
reader-wh94 <sstt9884@gmail.com>
2022-01-26 18:05:42 +0900

    Set update sweet

--- src/components/Sweet.js
+++ src/components/Sweet.js
@@ -1,25 +1,60 @@
+import React, { useState } from "react";
 import { dbService } from "fbase";
-import React from "react";
 
 const Sweet = ({ sweetObj , isOwner}) => {
+  const [editing, setEditing] = useState(false);
+  const [newSweet, setNewSweet] = useState(sweetObj.text);
+
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this sweet?");
     console.log(ok);
     if(ok) {
       await dbService.doc(`sweets/${sweetObj.id}`).delete();
     }
+  };
+
+  const toggleEditing = () => {
+    setEditing((prev) => !prev);
   }
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`sweets/${sweetObj.id}`).update({
+      text: newSweet,
+    });
+    setEditing(false);
+  }
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNewSweet(value);
+  };
 
   return (
     <div>
-      <h4>{sweetObj.text}</h4>
-      {isOwner && 
+      {
+        editing ? (
+          <>
+            <form onSubmit={onSubmit}>
+              <input type="text" placeholder="Edit your sweet" value={newSweet} required onChange={onChange}/>
+              <input type="submit" value="Update Sweet" />
+            </form>
+            <button onClick={toggleEditing}>Cancel</button>
+          </>
+        ) : (
         <>
-          <button onClick={onDeleteClick}>Delete Sweet</button>
-          <button>Edit Sweet</button>
+          <h4>{sweetObj.text}</h4>
+            {isOwner && 
+              <>
+                <button onClick={onDeleteClick}>Delete Sweet</button>
+                <button onClick={toggleEditing}>Edit Sweet</button>
+              </>
+            }
         </>
-      }
+        )}
     </div>
   );
 }
