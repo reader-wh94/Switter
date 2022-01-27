2a03bd409a7d46200e8395915669f06ad4f6a43e
reader-wh94 <sstt9884@gmail.com>
2022-01-27 20:09:25 +0900

    Delete isLoggedIn function

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,14 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user) {
-        setIsLoggedIn(true);
         setUserObj(user);
       } else {
-        setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
     });
@@ -21,7 +19,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "Initializing..."}
       <footer> &copy; {new Date().getFullYear()} Switter</footer>
     </>
   );
